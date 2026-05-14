a21d88349cde7a86672e31a3fd1a2e7abdf0bc49
Soulfly-ux <sirenar3171@gmail.com>
2026-05-14 16:09:05 +0200

    feat(exception-handling): add validation and generic error handlers

--- src/main/java/bg/footballbettingnotificationsvc/exception/GlobalExceptionHandler.java
+++ src/main/java/bg/footballbettingnotificationsvc/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package bg.footballbettingnotificationsvc.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -9,8 +10,22 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
       @ExceptionHandler(NotificationNotFoundException.class)
-    public ResponseEntity<String> handleNotificationNotFoundException(NotificationNotFoundException ex) {
+      public ResponseEntity<String> handleNotificationNotFoundException(NotificationNotFoundException ex) {
           return ResponseEntity.status(HttpStatus.NOT_FOUND)
                   .body(ex.getMessage());
+      }
+
+
+      @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleAnyException(Exception exception) {
+
+          return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                  .body("Unexpected server error");
+
+      }
+
+      @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException exception) { // Добре е параметър да е грешката, която обработваме
+          return ResponseEntity.badRequest().body("Invalid notification request");
       }
 }
